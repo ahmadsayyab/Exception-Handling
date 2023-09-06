8e5b1e7d0755905b4ad49a3e3dca0f06c6c40e0e
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 11:11:58 -0700

    Solved Exercise 3

---
+++ Prc_Exception_Handling/Exe_3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Exe_3
+    {
+        //3. Write a C# program that reads a file path
+        //from the user and tries to open the file. Handle exceptions if the file does not exist.
+
+
+        //Execise soultion
+        public static void filePath()
+        {
+
+            Console.Write("Input the file path: ");
+            string filePath = Console.ReadLine();
+            try
+            {
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+
+                    Console.WriteLine("File opened successfully.");
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occurred while accessing the file: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
+        }
+    }
+}
