1413bff27c0c1d7d9f657712f18a94669a33d263
Sayyab Khan <sayyabkhan007@gmail.com>
2023-09-06 05:22:04 -0700

    Solved Exercise 1

---
+++ Prc_Exception_Handling/Exe_1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Exe_1
+    {
+        //1. Write a C# program that prompts
+        //the user to input two numbers and divides them. Handle an exception when the user enters non-numeric values.
+
+        public static void DiviNumbers()
+        {
+
+
+
+            try
+            {
+                Console.WriteLine("Enter num1");
+                double num1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2");
+                double num2 = double.Parse(Console.ReadLine());
+
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                }
+                else
+                {
+                    double result = num1 / num2;
+                    Console.WriteLine(result);
+                }
+
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Cannot enter a non numeric  value");
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:{ex.Message}");
+            }
+
+        }
+
+    }
+}
